feeef4955c908914c4ebf9992b24a450e48f0c21
alanis <fkurushi@viona.rocks>
2024-08-12 14:49:38 +0200

    So many errors

--- obj/Debug/net8.0/SimpleChat.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimpleChat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df5273da69aca63bec8ae008f2841e8b6e4b8925")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e65aee166e47d2e865aabe208136a7666ca616c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
