4bad90ad6cc25155f3b73643aac021b3127dd3b0
alanis <fkurushi@viona.rocks>
2024-08-22 09:37:48 +0200

    braches clean up

--- obj/Debug/net8.0/SimpleChat.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimpleChat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b66b8c80832b85349801a2b9de6e7407e63d802d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d8f9f3eeaddf17aa1fd5e2f585eeebd61c60dd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
